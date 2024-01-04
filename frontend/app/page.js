c4081ff26e6ff8efab3e365627c5059c4a46d9af
Arham <arhamansari454647@gmail.com>
2024-01-04 18:55:39 +0500

    Home-Page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,3 +1,13 @@
+import React from "react";
+import Image from "next/image";
+import Nature from "../assets/images/Speakjourney_Map1.png";
+import styles from "./Home.module.css";
+
 export default function Home() {
-  return <h1>DashBoard</h1>;
+  return (
+    <div className={styles["nature-image"]}>
+      <Image src={Nature} layout="fill" objectFit="cover" quality={100} />
+      <h1 className="text-dark" style={{fontFamily : "cursive"}}>Your Language Odyssey begins here!</h1>
+    </div>
+  );
 }
