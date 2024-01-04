cd82ac304c629f55b32a6155603ec8d0a13bc15f
Arham <arhamansari454647@gmail.com>
2024-01-04 19:21:32 +0500

    Home-Page Almost Done

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -7,7 +7,26 @@ export default function Home() {
   return (
     <div className={styles["nature-image"]}>
       <Image src={Nature} layout="fill" objectFit="cover" quality={100} />
-      <h1 className="text-dark" style={{fontFamily : "cursive"}}>Your Language Odyssey begins here!</h1>
+      
+      <div className="container mt-lg-5">
+        <div className="row">
+          <div className="col">
+          <h1 className="text-dark" style={{ fontFamily: "cursive", fontWeight: "bolder" }}>
+          Your Language Odyssey begins here!
+        </h1>
+          </div>
+        </div>
+      </div>
+<br />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8" style={{ backgroundColor: "white", position: "relative", zIndex: 2 }}>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, corrupti repellendus cupiditate quibusdam in officia assumenda quod ea nisi provident harum at iste praesentium qui quia, obcaecati, odio maxime eos.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
